2fb6c691aa3c93314ba384d0e1b2e0f3f1ad79d4
Martin <martin.hovre@opencubes.io>
2017-08-22 11:36:58 +0200

    Rewiite of that has more functionality

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,61 @@
-declare class RequestBuilder {
-    static request(method: string, url: string): RequestBuilder
-    query(key: string, value: string, condition?: boolean | (() => boolean)): this
-    queries(queries: Object, condition?: boolean | (() => boolean)): this
-    header(name: string, value: string, condition?: boolean | (() => boolean)): this
-    headers(headers: Object, condition?: boolean | (() => boolean)): this
-    static get(url: string): RequestBuilder
-    static head(url: string): RequestBuilder
-    static post(url: string): RequestBuilder
-    static put(url: string): RequestBuilder
-    static delete(url: string): RequestBuilder
-    static connect(url: string): RequestBuilder
-    static options(url: string): RequestBuilder
-    static trace(url: string): RequestBuilder
-    static patch(url: string): RequestBuilder
-    send(body?: Buffer): Promise<Response>
+declare interface MultipartRequest {
+    query(key: string, value: string): MultipartRequest
+    query(queries: Object): MultipartRequest
+    set(name: string, value: string): MultipartRequest
+    set(headers: Object): MultipartRequest
+    attach(fieldName: string, data: Buffer, type: string, fileName?: string): MultipartRequest
+    send(): Promise<Response>
+}
+
+declare interface URLEncodedRequest {
+    query(key: string, value: string): URLEncodedRequest
+    query(queries: Object): URLEncodedRequest
+    set(name: string, value: string): URLEncodedRequest
+    set(headers: Object): URLEncodedRequest
+    add(key: string, value: string): URLEncodedRequest
+    add(fileds: Object): URLEncodedRequest
+    multipart(): MultipartRequest
+    send(): Promise<Response>
+}
+
+declare interface Request {
+    query(key: string, value: string): Request
+    query(queries: Object): Request
+    set(name: string, value: string): Request
+    set(headers: Object): Request
+    multipart(data: Object): URLEncodedRequest
+    multipart(): MultipartRequest
+    send(): Promise<Response>
+    send(data: Buffer): Promise<Response>
+    send(data: Array<Buffer>): Promise<Response>
+    pipe(readable: ReadableStream): Promise<Response>
+}
+
+declare var Request: {
+    new(method: string, url: string): Request
+    prototype: Request
+    get(url: string): Request
+    head(url: string): Request
+    post(url: string): Request
+    put(url: string): Request
+    del(url: string): Request
+    connect(url: string): Request
+    options(url: string): Request
+    patch(url: string): Request
 }
 
 declare class Response {
 
     readonly headers: Object
-    readonly status: number
-    readonly message: string
+    readonly statusCode: number
+    readonly statusMessage: string
+    readonly ok: boolean
 
+    readerRaw(): Promise<ReadableStream>
+    reader(): Promise<ReadableStream>
     buffer(): Promise<Buffer>
-
-    text(): Promise<string>
-
+    text(encoding?: BufferEncoding = 'utf-8'): Promise<string>
     json(): Promise<Object>
 }
 
-export = RequestBuilder
+export = Request
--- index.js
+++ index.js
@@ -1,105 +1,204 @@
-(define => define(() => {
+(define => define(['url', 'util', 'https', 'zlib'], ({ parse: parseURL }, util, https, zlib) => {
 
-const doIf = (condition, optional, success, faliure) => {
-    if (optional) {
-        if (typeof condition !== 'undefined') {
-            if (typeof condition === 'function') {
-                if (condition()) success&&success()
-                else faliure&&faliure()
-            } else if (condition) success&&success()
-            else faliure&&faliure()
-        } else success&&success()
-    } else {
-        if (typeof condition !== 'undefined') {
-            if (typeof condition === 'function') {
-                if (condition()) success&&success()
-                else faliure&&faliure()
-            } else if (condition) success&&success()
-            else faliure&&faliure()
-        } else faliure&&faliure()
-    }
+function includes(src, sub) {
+    if (!util.isArray(src) && (typeof src === 'object' || typeof src === 'function')) return sub in src
+    return !!~src.indexOf(sub)
 }
 
-class RequestBuilder {
-    constructor(method, url) {
-        const { host, path, port, protocol } = require('url').parse(url)
-        this._httpObj = require(protocol.substring(0, protocol.length - 1))
-        this._opts = { method, host, path, port, protocol }
-    }
-
-    query(key, value, condition) {
-        doIf(condition, true, () => {
-            if (Array.isArray(value)) for (let val of value)
-                this._opts.path += `${this._opts.path.includes('?') ? '&' : '?'}${encodeURIComponent(key)}=${encodeURIComponent(val)}`
-            else this._opts.path += `${this._opts.path.includes('?') ? '&' : '?'}${encodeURIComponent(key)}=${encodeURIComponent(value)}`
-        })
-        return this
-    }
-
-    queries(queries, condition) {
-        doIf(condition, true, () => {
-            Array.from(Object.keys(queries)).filter(queries.hasOwnProperty).map(key => ({ key, value: queries[key] })).forEach(({ key, value }) => this.query(key, value))
-        })
-        return this
-    }
-
-    header(name, value, condition) {
-        doIf(condition, true, () => this._opts.headers = Object.assign({ [name]: value }, Object(this._opts.headers)))
-        return this
-    }
-
-    headers(headers, condition) {
-        doIf(condition, true, () => this._opts.headers = Object.assign(Object(this._opts.headers), Object(headers)))
-        return this
-    }
-
-    send(body) {
-        return new Promise((resolve, reject) => {
-            const req = this._httpObj.request(this._opts, res => {
-                const bufs = []
-                res.on('data', d => bufs.push(d))
-                res.on('end', () => {
-                    resolve(new Response(Buffer.concat(bufs), res.headers, res.statusCode, res.statusMessage))
-                })
-            })
-            req.end(body)
-        })
-    }
-
-    static request(method, url) { return new RequestBuilder(method, url) }
-
-    static get(url) { return RequestBuilder.request('GET', url) }
-    static head(url) { return RequestBuilder.request('HEAD', url) }
-    static post(url) { return RequestBuilder.request('POST', url) }
-    static put(url) { return RequestBuilder.request('PUT', url) }
-    static delete(url) { return RequestBuilder.request('DELETE', url) }
-    static options(url) { return RequestBuilder.request('OPTIONS', url) }
-    static patch(url) { return RequestBuilder.request('PATCH', url) }
+function Request(method, url) {
+    this.request = Object.assign({}, parseURL(url), { method, headers: {} })
+    this.request.search = this.request.search || ''
 }
 
-class Response {
-    constructor(buffer, headers, status, message) {
-        this._buffer = buffer
-        this._headers = headers
-        this._status = status
-        this._message = message
+Request.prototype.query = function query(key, value) {
+    if (typeof key === 'object') {
+        Object.getOwnPropertyNames(key).forEach(k => this.query(k, key[k]))
+        return this
     }
+    
+    if (typeof key === 'object' || typeof key === 'function' || typeof key === 'boolean', typeof key === 'undefined')
+        throw new TypeError(`Invalid key type: ${typeof key}`)
+    
+    if (typeof value === 'function' || (typeof value === 'object' && !util.isArray(value)))
+        throw new TypeError(`Invalid value type: ${typeof value}`)
 
-    get headers() { return this._headers }
-    get status() { return this._status }
-    get message() { return this._message }
-
-    buffer() {
-        return Promise.resolve(this._buffer)
-    }
-
-    text() {
-        return Promise.resolve(this._buffer.toString())
-    }
-
-    json() {
-        return this.text().then(JSON.parse)
-    }
+    if (Array.isArray(value)) for (let val of value)
+        this.request.search  += `${this.request.search[0] === '?' ? '&' : '?'}${encodeURIComponent(key)}=${encodeURIComponent(val)}`
+    else this.request.search += `${this.request.search[0] === '?' ? '&' : '?'}${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+    return this
 }
-return RequestBuilder
-}))(typeof define === 'function' && define.amd ? define : factory => module.exports = factory())
+
+Request.prototype.set = function set(name, value) {
+    if (typeof name === 'object') {
+        for (let key of Object.getOwnPropertyNames(name)) this.set(key, name[key])
+        return this
+    }
+
+    if (!util.isString(name))
+        throw new TypeError(`Invalid name type: ${typeof name}`)
+
+    this.request.headers[name] = value.toString()
+    return this
+}
+
+Request.prototype.multipart = function multipart(data) {
+    if (data) {
+        const req = new URLEncodedRequest()
+        return
+    }
+    return new MultipartRequest(this.request)
+}
+
+Request.prototype.send = function send(data) {
+    return new Promise((resolve, reject) => {
+        const req = {
+            host: this.request.host,
+            path: this.request.path + this.request.search + (this.request.hash || ''),
+            headers: this.request.headers,
+            method: this.request.method,
+            port: this.request.port || this.request.protocol === 'https:' ? 443 : 80
+        }
+        if (this.request.auth)
+            req['auth'] = this.request.auth
+        const request = https.request(req, res => {
+            resolve(new Response(res))
+        })
+        if (data) {
+            if (util.isArray(data)) for (let d of data)
+                request.write(d)
+            else request.write(data)
+        }
+        request.end()
+    })
+}
+
+function MultipartRequest(request) {
+    this.request = request
+    this.bufs = []
+}
+
+MultipartRequest.boundary = '--MultipartRequest'
+
+MultipartRequest.prototype.query = Request.prototype.query
+MultipartRequest.prototype.set = Request.prototype.set
+MultipartRequest.prototype.attach = function attach(fieldName, data, type, fileName) {
+    if (data === void 0)
+        return this
+    let head = `\r\n--${MultipartRequest.boundary}\r\nContent-Disposition: form-data; name="${fieldName}"`
+    if (filename)
+        head += `; filename="${filename}"`
+    head += `\r\nContent-Type: ${type || 'application/octet-stream'}`
+    this.bufs.push(
+        new Buffer(head + '\r\n\r\n'),
+        data
+    )
+    return this
+}
+MultipartRequest.prototype.send = function send() {
+    this.bufs.push(new Buffer(`\r\n--${MultipartRequest.boundary}--`))
+    this.request.headers['Content-Type'] = `multipart/form-data; boundary=${MultipartRequest.boundary}`
+    return Request.prototype.send.call(this, this.bufs)
+}
+
+function URLEncodedRequest(request) {
+    this.request = request
+    this.buf = ''
+}
+
+URLEncodedRequest.prototype.boundary = '--URLEncodedRequest'
+
+URLEncodedRequest.prototype.query = Request.prototype.query
+URLEncodedRequest.prototype.set = Request.prototype.set
+URLEncodedRequest.prototype.multipart = function multipart() {
+    const req = new MultipartRequest(this.request)
+    const { query } = parseURL(this.buf, true)
+    Object.getOwnPropertyNames(query).forEach(key => {
+        req.attach(
+            key,
+            util.isArray(query[key]) ? JSON.stringify(query[key]) : query[key],
+            util.isArray(query[key]) ? 'application/json' : 'text/plain'
+        )
+    })
+    return req
+}
+URLEncodedRequest.prototype.add = function add(key, value) {
+    if (typeof key === 'object') {
+        Object.getOwnPropertyNames(key).forEach(k => this.add(k, key[k]))
+        return this
+    }
+    
+    if (typeof key === 'object' || typeof key === 'function' || typeof key === 'boolean', typeof key === 'undefined')
+        throw new TypeError(`Invalid key type: ${typeof key}`)
+    
+    if (typeof value === 'function' || (typeof value === 'object' && !util.isArray(value)))
+        throw new TypeError(`Invalid value type: ${typeof value}`)
+
+    if (Array.isArray(value)) for (let val of value)
+        this.request.search  += `${this.request.search[0] === '?' ? '&' : '?'}${encodeURIComponent(key)}=${encodeURIComponent(val)}`
+    else this.request.search += `${this.request.search[0] === '?' ? '&' : '?'}${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+    return this
+}
+URLEncodedRequest.prototype.send = function send() {
+    this.buf = this.buf.substring(1)
+    this.request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    return Request.prototype.send.call(this, this.buf)
+}
+function Response(res) {
+    this.stream = res
+    this.headers = res.headers
+    this.statusCode = res.statusCode,
+    this.statusMessage = res.statusMessage
+    this.ok = 200 <= res.statusCode < 300
+}
+
+Response.prototype.statusMessage = ''
+Response.prototype.statusCode = 0
+Response.prototype.ok = false
+
+Response.prototype.readerRaw = function reader() { return Promise.resolve(this.stream) }
+Response.prototype.reader = function reader() {
+    return new Promise(resolve => {
+        const encoding = this.headers['content-encoding'] || ''
+        let stream
+        if (!encoding)
+            stream = this.stream
+        else if (includes(encoding, 'gzip')) {
+            stream = zlib.createGunzip()
+            stream.pipe(this.stream)
+        }
+        else if (includes(encoding, 'deflate')) {
+            stream = zlib.createDeflate()
+            stream.pipe(this.stream)
+        }
+        resolve(stream)
+    })
+}
+Response.prototype.buffer = function buffer() { return this.reader().then(reader => new Promise(resolve => {
+    const data = []
+    let totalLength = 0
+    reader
+        .on('data', d => {data.push(d);totalLength += d.length})
+        .once('end', () => resolve(Buffer.concat(data, totalLength)))
+}))}
+Response.prototype.text = function text(enconding) { return this.buffer().then(buf => buf.toString(enconding || 'utf-8')) }
+Response.prototype.json = function json() { return this.text().then(JSON.parse) }
+
+function request(method, url) {
+    return new Request(method, url)
+}
+['get', 'post', 'put', 'patch', 'head', 'options'].forEach(method => {
+    request[method] = function (url) {
+        return new Request(method.toUpperCase(), url)
+    }
+})
+request.del = function (url) {
+    return new Request('DELETE', url)
+}
+return request
+}))(typeof define === 'function' && define.amd ? define : (deps, factory) => {
+    if (typeof deps === 'function') {
+        factory = deps
+        deps = []
+    }
+    module.exports = factory.apply(null, deps.map(require))
+})
