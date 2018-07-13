e74f2ad69c5ef1fbb919dfdad02f991cf804e817
ocpu <martin.hovre@opencubes.io>
2018-07-13 18:47:05 +0200

    Most of the rewrite done

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,65 @@
-import { Readable } from "stream";
+import { Duplex, Readable } from "stream";
 import { Brev } from "brev";
-import { IncomingMessage } from "http";
+import { IncomingHttpHeaders } from "http";
+import { WriteStream } from "fs";
 
-interface EventMap {
-  "response": IncomingMessage
+declare interface EventMap {
+  "response": Response
   "data": Buffer
   "end": void
   "close": void
   "readable": void
   "error": Error
 }
+declare interface Response {
+  readonly code: number
+  readonly stream: Readable
+  readonly headers: IncomingHttpHeaders
+  buffer(): Promise<Buffer>
+  text(encoding?: BufferEncoding): Promise<string>
+  json<T = any>(): Promise<T>
+}
+declare interface RequestOptions {
+  followRedirects?: boolean
+  method?: string
+}
+declare function request(url: string, opts?: RequestOptions): request.Request
+declare namespace request {
+  export var supportedEncodings: string[]
+  export function request(url: string, opts?: RequestOptions): request.Request
+  export interface Request extends Brev<EventMap>, Duplex {
+    go(): Promise<Response>
+    go(callback: (res: Response) => void): void
+    json<T = any>(): Promise<T>
+    json(data: any): Promise<Response>
+    json(data: any, callback: (res: Response) => void): void
+    set<K extends keyof IncomingHttpHeaders>(name: K, value: string | string[]): Request
+    set(pairs: IncomingHttpHeaders): Request
+    query(name: string, value: string | string[]): Request
+    query(pairs: { [name: string]: string | string[] }): Request
+    clone(): Request
+    toFile(file: string, encoding?: BufferEncoding): WriteStream
+  }
+  interface FormData {
+    readonly stream: Readable
+    conteentType: string
+    attach(name: string, value: string): FormData
+    attach(data: {[name: string]: string}): FormData
+  }
+  interface Multipart {
+    readonly stream: Readable
+    conteentType: string
+    attach(field: string, data: FormData): Multipart
+    attach(field: string, data: Multipart): Multipart
+    attach(field: string, data: Object, contentType?: string, filename?: string): Multipart
+    attach(field: string, data: Readable, contentType?: string, filename?: string): Multipart
+    attach(field: string, data: Buffer, contentType?: string, filename?: string): Multipart
+    attach(field: string, data: string, contentType?: string, filename?: string): Multipart
+    attach(field: string, data: number, contentType?: string, filename?: string): Multipart
+  }
+  export function formData(data?: {[name: string]: string}): FormData
+  export function multipart(): Multipart
+}
 
-declare function request(url: string): {
-  go(): Promise<IncomingMessage>
-  go(callback: (IncommingMessage) => void): void
-  set(name: string, value: string | string[]): this
-  set(pairs: { [name: string]: string | string[] }): this
-  setHeader(name: string, value: string | string[]): this
-  setHeaders(pairs: { [name: string]: string | string[] }): this
-  compress(...order: ('identity' | 'gzip' | 'deflate' | 'compress' | 'br')[]): this
-  decompress(): this
-} & Brev<EventMap> & Readable
 
 export = request
--- index.js
+++ index.js
@@ -1,79 +1,170 @@
-const { Duplex, PassThrough } = require('stream')
+const { Duplex, PassThrough, Readable } = require('stream')
+const { createGunzip, createDeflate } = require('zlib')
 const brev = require('brev')
 const d = require('d')
 const autoBind = require('d/auto-bind')
 
+let decompressStream, LZWDecoder
+try { decompressStream = require('iltorb').compressStream } catch (_) {}
+try { LZWDecoder = require('lzw-stream').LZWDecoder } catch (_) {}
+
+
+const supportedEncodings = ['identity', 'gzip', 'deflate']
+if (decompressStream) supportedEncodings.push('compress')
+if (LZWDecoder) supportedEncodings.push('br')
+const response = res => ({
+  /** @type {Readable} */
+  stream: (() => {return decompress(res.pipe(new PassThrough),res.headers['content-encoding'])})(),
+  get headers() {return res.headers},
+  get code() {return res.statusCode},
+  buffer(){let a=[],l=0;return new Promise(r=>this.stream.on('data',c=>(void a.push(c))||(l+=c.length)).once('end',_=>r(Buffer.concat(a,l))))},
+  text(encoding='utf8') {return this.buffer().then(buf => buf.toString(encoding))},
+  json() {return this.text().then(JSON.parse)},
+})
 const request = (url, options) => brev().mixin((() => {
-  const extra = Object.assign({}, options)
+  const extra = Object.assign({followRedirects:true}, options)
   const urlParsed = require('url').parse(url, true)
   const stream = new Duplex
+  let i = 0
   Object.defineProperties(stream, autoBind({
     _read: d(function () {
-      this.on('response', res => {
-        let pushedNull = false
-        res.on('data', c => {
-          pushedNull = c === null
-          stream.push(c)
-        }).on('end', () => !pushedNull && stream.push(null))
-      })
-      stream.go()
+      this.once('response', res =>
+        (i++ === 0)&&res.stream.on('data', c => stream.push(c)).once('end', stream.push)
+      )
+      send()
     }),
     _write: d(function (chunk, _, cb) {
       if (!stream._req) stream._req = require((urlParsed.protocol || 'http:').slice(0, -1)).request(
         Object.assign({ method: 'POST' }, urlParsed, extra),
-        res => this.emit('response', res)
-      ); else stream._req.write(chunk)
+        res => this.emit('response', response(res))
+      )
+      stream._req.write(chunk)
       cb()
     }),
     _final: d(function (cb) {
       stream._req.end()
       cb()
     }),
-    go: d(function (cb) {
-      require((urlParsed.protocol || 'http:').slice(0, -1)).request(
-        Object.assign({ method: 'GET' }, urlParsed, extra),
-        res => this.emit('response', res)
-      ).end()
-      if (cb) return this.once('response', cb)
-      return new Promise(resolve => {
-        this.once('response', resolve)
-      })
+    json: d(function (data, cb) {
+      if (data === void 0) return stream.go().then(res => res.json())
+      if (typeof data === 'function') return stream.go(res => res.json().then(data))
+      if (extra.headers?!Object.keys(extra.headers).map(s=>s.toLowerCase()).includes('accept-encoding'):true)
+        stream.set('Content-Type', 'application/json')
+      stream.write(JSON.stringify(data))
+      return cb ? stream.on('response', cb) : stream.once('response')
     }),
-    decompress: d(function() {
-
+    go: d(function (cb) {
+      return send(void 0, cb)
     })
   }))
-  stream.set = stream.setHeaders = stream.setHeader = function (key, value) {
+  const send = (data, cb) => {
+    req(require((urlParsed.protocol || 'http:').slice(0, -1)), Object.assign({ method: 'GET' }, urlParsed, extra), stream, data)
+    if (cb) return stream.once('response', cb)
+    return new Promise(resolve =>
+      stream.once('response', resolve)
+    )
+  }
+  stream.set = function (key, value) {
     extra.headers = extra.headers || {}
-    if (arguments.length == 2) {
-      if (key in extra.headers)
-        Array.isArray(value) ? extra.headers.concat(value) : extra.headers.push(value)
-      else extra.headers[key] = Array.isArray(value) ? value : [value]
-    } else {
-      for (const name in key) if (key.hasOwnProperty(name)) {
-        if (name in extra.headers)
-          Array.isArray(value) ? extra.headers.concat(value) : extra.headers.push(value)
-        else extra.headers[name] = Array.isArray(value) ? value : [value]
+    if (typeof key !== 'object') extra[key] = Array.isArray(value) ? value.join(', ') : value
+    else Object.keys(key).forEach(k => extra[k] = Array.isArray(key[k]) ? key[k].join(', ') : key[k])
+  }
+  stream.query = function (key, value) {
+    extra.query = extra.query || {}
+    if (typeof key !== 'object') extra[key] = Array.isArray(value) ? value.join(', ') : value
+    else Object.keys(key).forEach(k => extra[k] = Array.isArray(key[k]) ? key[k].join(', ') : key[k])
+  }
+  stream.clone = () => request(url, extra)
+  stream.toFile = (file, encoding='utf8') => stream.pipe(require('fs').createWriteStream(file, encoding))
+  return stream
+})())
+
+const encodingToStream = encoding => 
+  encoding === 'gzip' ? createGunzip() :
+  encoding === 'deflate' ? createDeflate() :
+  encoding === 'compress' && LZWDecoder !== void 0 ? new LZWDecoder :
+  encoding === 'br' && decompressStream !== void 0 ? decompressStream() :
+  new PassThrough
+const decompress = (stream, contentEncoding) => {
+  if (!contentEncoding) return stream
+  const streams = contentEncoding.split(/,\s?/g).map(encodingToStream)
+  let decompressedStream = streams.pop()
+  if (streams.length) streams.reduceRight(
+    (stream, c) => stream.pipe(c),
+    decompressedStream
+  )
+  stream.pipe(decompressedStream)
+  return decompressedStream
+}
+const req = (mod, obj, self, data) => !obj.followRedirects ? mod.request(obj, self.emit('response', res)).end(data) : mod.request(
+  obj,
+  res => res.statusCode === 301 || res.statusCode === 307
+    ? req(mod, Object.assign({}, obj, require('url').parse(res.headers['location'], true)), self, data)
+    : self.emit('response', response(res))
+).end(data)
+
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+const nonce = (n=16) => new Array(n).fill('').map(() => chars[Math.random() * chars.length | 0]).join('')
+
+request.supportedEncodings = supportedEncodings
+request.request = request
+request.formData = (data) => {
+  const stream = new PassThrough
+  let firstChunkWritten = false
+  if (data) {
+    firstChunkWritten = true
+    stream.write(Object.keys(data).reduce((a, key) => `${a?a+'&':''}${key}=${data[key]}`, ''))
+  }
+  return {
+    get stream() { return stream },
+    contentType: 'application/x-www-form-urlencoded',
+    attach(name, value) {
+      if (typeof name === 'object') {
+        stream.write(Object.keys(data).reduce((a, key) => `${firstChunkWritten?'':a?a+'&':''}${key}=${data[key]}`, ''))
+        firstChunkWritten = true
+      } else if (firstChunkWritten) {
+        stream.write(`&${name}=${value}`)
+      } else {
+        firstChunkWritten = true
+        stream.write(`${name}=${value}`)
       }
     }
   }
-  return stream
-})())
-module.exports = request
-// const stream = new Writable
-// stream._write = function (chunk, encoding, cb) {
-//   console.log(chunk.toString())
-//   cb()
-// }
-// stream._final = function (cb) {
-//   console.log('end')
-//   cb()
-// }
+}
+request.multipart = () => {
+  const boundary = nonce(8)
+  const stream = new PassThrough
+  stream.end = (d, e, cb) => {
+    stream.write('--' + boundary + '--')
+    PassThrough.prototype.end.call(stream, d, e, cb)
+  }
+  return {
+    get stream() { return stream },
+    contentType: 'multipart/form-data; boundary=' + boundary,
+    attach(field, value, contentType, filename) {
+      stream.write('--' + boundary + '\r\n')
+      if (typeof value.stream === 'object' && typeof value.contentType === 'string' && typeof value.attach === 'function') {
+        stream.write(`Content-Disposition: ${value.contentType}; name="${field}"${contentType&&(`; filename="${contentType}"`)}\r\n`)
+        stream.pipe(value.stream , {end:false})
+      } else if (typeof value === 'object') {
+        if (value instanceof Buffer) {
+          stream.write(`Content-Disposition: ${contentType || 'application/octet-stream'}; name="${field}"${filename&&(`; filename="${filename}"`)}\r\n`)
+          stream.write(value)
+        } else if (require('is-stream')(value)) {
+          stream.write(`Content-Disposition: ${contentType || 'application/octet-stream'}; name="${field}"${filename&&(`; filename="${filename}"`)}\r\n`)
+          stream.pipe(value, {end:false})
+        } else {
+          stream.write(`Content-Disposition: ${contentType || 'application/json'}; name="${field}"${filename&&(`; filename="${filename}"`)}\r\n`)
+          stream.write(JSON.stringify(value))
+        }
+      } else {
+        stream.write(`Content-Disposition: ${contentType || 'text/plain'}; name="${field}"${filename&&(`; filename="${filename}"`)}\r\n`)
+        stream.write(value.toString())
+      }
+      stream.write('\r\n\r\n')
+      return this
+    }
+  }
+}
 
-// const test = new PassThrough()
-// test.pipe(stream)
-// test.write('test')
-// test.write('test')
-// test.write('test')
-// test.write('test')
-// test.end()
+module.exports = request
